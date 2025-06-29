33249505efad39db804708c374e33575f38c92bf
saikrishh <52971735+saikrishh@users.noreply.github.com>
2025-06-29 14:04:54 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Replace with your Firebase project config
+export const firebaseConfig = {
+  apiKey: "AIzaSyBTOcjPeXYW5aghd4Vm0aG0eTeHaTmGpeM",
+  authDomain: "weblogin-25786.firebaseapp.com",
+  projectId: "weblogin-25786",
+  storageBucket: "weblogin-25786.firebasestorage.app",
+  messagingSenderId: "578750022804",
+  appId: "1:578750022804:web:83863549331d38526de081"
+};
